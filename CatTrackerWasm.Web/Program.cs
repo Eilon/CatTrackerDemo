da652226147b3a36b6893e0799d6eef27828b87c
Husam Ebish <husam.ebish@gmail.com>
2021-05-14 06:08:35 +0200

    Added WebAsem project

---
+++ CatTrackerWasm.Web/Program.cs
@@ -0,0 +1,24 @@
+using CatTrackerDemo;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using App = CatTrackerDemo.WebUI.App;
+
+namespace CatTrackerWasm.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<TrackerState>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
